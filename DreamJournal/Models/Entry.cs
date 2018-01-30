f3b17646b36cb8adf34f7c39b9ee68929c9913d0
James <jamescarlosborn@gmail.com>
2018-01-30 13:55:51 -0800

    add Models/Entry.cs

---
+++ DreamJournal/Models/Entry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace DreamJournal.Models
+{
+    [Table("Entries")]
+    public class Entry
+    {
+        [Key]
+        public int EntryId { get; set; }
+        public string Author { get; set; }
+        public string Body { get; set; }
+        public DateTime SleepTime { get; set; }
+        public DateTime WakeTime { get; set; }
+        public string PreSleepInfo { get; set; }
+        public bool CatNap { get; set; }
+        public bool WasLucid { get; set; }
+        public string LucidMoment { get; set; }
+        public string Tags { get; set; }
+        public string SelfAnalysis { get; set; }
+    }
+}
