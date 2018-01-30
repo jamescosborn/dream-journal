0fc237d733dd5347a25855a9e84c136f0c1f45a8
James <jamescarlosborn@gmail.com>
2018-01-30 15:46:46 -0800

    update DreamJournalContext.cs

--- DreamJournal/Models/DreamJournalContext.cs
+++ DreamJournal/Models/DreamJournalContext.cs
@@ -6,9 +6,10 @@ namespace DreamJournal.Models
     public class DreamJournalContext : DbContext
     {
         public DbSet<Entry> Entries { get; set; }
+        public DbSet<Interpretation> Interpretations { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder
-                .UseMySql(@"Server=localhost;Port=8889;database=dreamjournal;uid=root;pwd=root;");
+                .UseMySql(@"Server=localhost;Port=8889;database=DreamJournal;uid=root;pwd=root;");
     }
 }
