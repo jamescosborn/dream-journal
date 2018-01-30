6916e4b2c4c5aa2e894039b0a8e641a0beccd8d8
James <jamescarlosborn@gmail.com>
2018-01-30 14:08:38 -0800

    update Startup.cs, Entry.cs, add Entries/Index.cshtml

--- DreamJournal/Models/Entry.cs
+++ DreamJournal/Models/Entry.cs
@@ -6,6 +6,7 @@ namespace DreamJournal.Models
     {
         [Key]
         public int EntryId { get; set; }
+        public string Title { get; set; }
         public string Author { get; set; }
         public string Body { get; set; }
         public DateTime SleepTime { get; set; }
