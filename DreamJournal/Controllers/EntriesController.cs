225ba04f947251a72df73c936b7ef47c713d1cd3
James <jamescarlosborn@gmail.com>
2018-01-30 14:05:16 -0800

    add Controllers/EntriesController

---
+++ DreamJournal/Controllers/EntriesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DreamJournal.Models;
+using System.Linq;
+
+namespace DreamJournal.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private DreamJournalContext db = new DreamJournalContext();
+
+        public IActionResult Index()
+        {
+            List<Entry> model = db.Entries.ToList();
+            return View();
+        }
+    }
+}
